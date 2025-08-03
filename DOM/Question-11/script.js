e5568cc12a98b76babb1840331c65a5f7d6d01ae
itsmeneha007 <nehabaghel11092003@gmail.com>
2025-08-03 21:23:32 +0530

    DOM-question-11

---
+++ DOM/Question-11/script.js
@@ -0,0 +1,9 @@
+const heading = document.getElementById("main-heading");
+heading.textContent = "Welcome to the DOM World!";
+
+const paragraphs = document.getElementsByTagName("p");
+for (let i = 0; i < paragraphs.length; i++) {
+  paragraphs[i].style.color = "blue";
+}
+const containerDiv = document.querySelector(".container");
+containerDiv.style.backgroundColor = "yellow";
